506ec44c342aca5e1a752edd16483d2a3f3055ef
Kristine Xu <xu.cupcake.kristine@gmail.com>
2015-06-08 12:26:47 -0400

    Worked on Verbs.java

--- src/Verbs.java
+++ src/Verbs.java
@@ -1,4 +1,28 @@
+/**@author Kristine and Ashley
+ * Work in progress
+*/ 
 
-public class Verbs {
-
+public class Verbs extends Resource {
+	
+	public Verbs(String name)
+	{
+		super(name);
+	}
+	
+	public Verbs(String name, int number)
+	{
+		super(name, number);
+	}
+	
+	public void doHomework()
+	{
+		
+	}
+	
+	public void collectPencils()
+	{
+	 
+	}
+	
+	
 }
