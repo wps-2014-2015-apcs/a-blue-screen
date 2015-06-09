9c3030b5efe2f8fe72011907eacda10bb8b3677a
haelmic3 <haelmic3@gmail.com>
2015-06-08 22:50:37 -0400

    Update Map.java
    I want input?
    We still need to generate map data.
    We still n...

--- src/Map.java
+++ src/Map.java
@@ -4,15 +4,38 @@
 |*|
 \*/
 public class Map {
-  int[][] grid;
-  //int[][] screen;
-  int playerX;
-  int playerY;
+  public enum Move{FOWARD, BACKWARD, LEFT, RIGHT};
+  public String[][] grid;
+//public int[][] screen;
+  public int playerX;
+  public int playerY;
   
-  public int[][] getData(){
-    return null;
+  public Map(){
+    grid = new String[63][63];
+    playerX = 32;
+    playerY = 32;
   }
+  
+  public String[][]getData(){return grid;}
+  public int getPlayerX(){return playerX;}
+  public int getPlayerY(){return playerY;}
+  
+  public void movePlayer(String d){
+    d = d.toUpperCase();
+    if (d == "FORWARD" || d == "UP") playerY--;
+    if (d == "BACKWARD" || d == "DOWN") playerY++;
+    if (d == "LEFT") playerX--;
+    if (d == "RIGHT") playerX++;
+  }
+  
   public String toString(){
-    return "";
+    String ans = "";
+    for(String[] i : grid){
+      for(String s : i){
+        ans += i;
+      }
+      ans += "\n"
+    }
+    return ans;
   }
 }
