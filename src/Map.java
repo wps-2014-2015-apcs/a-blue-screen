f5f59fbf6786d7bba9ad039c2ca73017c19dbe95
haelmic3 <haelmic3@gmail.com>
2015-06-09 14:03:22 -0400

    Map.java Updated
    My comment before was incorrect it was 18.5 not 24.5 anyway checkData(){/*gets all points in a four step radius and tests them for loadedness. This could be one of two things ether the map is hidden by a screen and the data is there or the data has not been generated yet  no need for a screen. i like the second but that might mean we need a list of places we need and add them in Random order to grid.  If the location has been loaded it should do nothing. in this aspect having a screen would be easier because we would just show what is under screen but where is the fun in that*/}

--- src/Map.java
+++ src/Map.java
@@ -17,16 +17,14 @@
 
 public class Map {
   public String[][] grid;
-//public int[][] screen;
+//public int[][] screen
   public int playerX;
   public int playerY;
   
-  public Map(){
-    grid = new String[63][63];
-    playerX = 32;
-    playerY = 32;
-  }
+  public Map () { grid = new String[63][63];playerX = 32; playerY = 32; }
+  public Map (String[][] grid) { this.grid = grid; playerX = (grid.size+1)/2; playerY = (grid[0].size+1)/2; }
   
+  public String getDataPoint(int x, int y){return grid[x][y];}
   public String[][]getData(){return grid;}
   public int getPlayerX(){return playerX;}
   public int getPlayerY(){return playerY;}
@@ -37,13 +35,14 @@ public class Map {
     if (d == "BACKWARD" || d == "DOWN") playerY++;
     if (d == "LEFT") playerX--;
     if (d == "RIGHT") playerX++;
+    checkData()
   }
-  
+  public void checkData(){}
   public String toString(){
     String ans = "";
     for(String[] i : grid){
       for(String s : i){
-        ans += i;
+        ans += i.equals("") ? " " : i;
       }
       ans += "\n";
     }
