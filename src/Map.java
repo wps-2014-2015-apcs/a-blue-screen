e5b2a54c4ed62d16eeaa848631f487416a464860
haelmic3 <haelmic3@gmail.com>
2015-06-08 21:27:01 -0400

    Update Map.java
    Do you agree with this?

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,18 @@
-/*
- * 
- * @author Mike, Billy, Will
- * 
- */
+/*\
+|*|
+|*|@author Michael, Billy, Will
+|*|
+\*/
 public class Map {
-
+  int[][] grid;
+  //int[][] screen;
+  int playerX;
+  int playerY;
+  
+  public int[][] getData(){
+    return null;
+  }
+  public String toString(){
+    return "";
+  }
 }
