feaa0a8039c2cfd5809382ac96a5353defddea74
Jyu25 <farmeryu@gmail.com>
2015-06-09 09:19:58 -0400

    Created Storyline class

---
+++ src/Storyline.java
@@ -0,0 +1,15 @@
+/*
+ * 2014-2015 APCS "A Blue Screen" Project
+ * 
+ * @author Lukas Goodman
+ * @author Jeremy Yu
+ * @author Lucy Dong
+ * @author Jefferson Kim	
+ * 
+ *
+ * Outlines Story/Plot of the game
+ * Contains project text
+ */
+public class Storyline {
+
+}
