085eab62d1923417d098e54ed0d3d97310761347
Kristine Xu <xu.cupcake.kristine@gmail.com>
2015-06-09 09:42:28 -0400

    Verbs class added

--- src/Verbs.java
+++ src/Verbs.java
@@ -15,9 +15,29 @@
  * 
  */
 
-public class Verbs {
+/**
+ * The verbs of the project. 
+ */
+public class Verbs extends Resource {
+	
 	public Verbs(String name)
+	{
+		super(name);
+	}
+	
+	public Verbs(String name, int number)
+	{
+		super(name, number);
+	}
+	
+	public void doHomework()
 	{
 		
 	}
+	
+	public void collectPencils()
+	{
+	 
+	}
+	
 }
