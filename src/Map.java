b839c6b5d3c73a240251573fbeea77cf5a849c6c
haelmic3 <haelmic3@gmail.com>
2015-06-12 18:21:46 -0400

    Update Map.java
    formatting and added print after every step

--- src/Map.java
+++ src/Map.java
@@ -20,7 +20,6 @@
  *
  */
 public class Map {
-  public enum Move{FOWARD, BACKWARD, LEFT, RIGHT};
   public String[][] grid;
   public String[][] gridNoCharacter;
 //public int[][] screen;
@@ -28,68 +27,46 @@ public class Map {
   public int playerY;
   
   public Map(){
-	 final int SIZE = 21;
-	 int middle = (SIZE - 1) / 2;
-	
-	playerX = middle;
-    playerY = middle;
+    final int SIZE = 21;
+    int m = (SIZE - 1) / 2;
+    playerX = m; playerY = m;
     grid = new String[SIZE][SIZE];
-    
-    for(int r = 0; r < SIZE; r++)
-    	for(int c = 0; c < SIZE; c++)
-    		grid[r][c] = "*";
-    
+    for(int r = 0; r < SIZE; r++) for(int c = 0; c < SIZE; c++) grid[r][c] = "*";
     grid[middle][middle] = "@";
-    
     int randRow;
     int randCol;
     for(int i = 0; i < 5; i++)// 5 Friends Houses
     {
-    	randRow = (int)(Math.random() * SIZE);
-    	randCol = (int)(Math.random() * SIZE);
-    	if(grid[randRow][randCol].equals("*"))
-    		grid[randRow][randCol] = "H";
-    	else
-    		i--;
+      randRow = (int)(Math.random() * SIZE);
+      randCol = (int)(Math.random() * SIZE);
+      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "H";
+      else i--;
     }
-    
     for(int i = 0; i < 5; i++)// 5 Coffee Shops
     {
-    	randRow = (int)(Math.random() * SIZE);
-    	randCol = (int)(Math.random() * SIZE);
-    	if(grid[randRow][randCol].equals("*"))
-    		grid[randRow][randCol] = "C";
-    	else
-    		i--;
+      randRow = (int)(Math.random() * SIZE);
+      randCol = (int)(Math.random() * SIZE);
+      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "C";
+      else i--;
     }
-    
     for(int i = 0; i < 5; i++)// 5 Office supply Stores
     {
-    	randRow = (int)(Math.random() * SIZE);
-    	randCol = (int)(Math.random() * SIZE);
-    	if(grid[randRow][randCol].equals("*"))
-    		grid[randRow][randCol] = "O";
-    	else
-    		i--;
+      randRow = (int)(Math.random() * SIZE);
+      randCol = (int)(Math.random() * SIZE);
+      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "O";
+      else i--;
     }
-    
     for(int i = 0; i < 1; i++)// 1 School
     {
-    	randRow = (int)(Math.random() * SIZE);
-    	randCol = (int)(Math.random() * SIZE);
-    	if(grid[randRow][randCol].equals("*"))
-    		grid[randRow][randCol] = "S";
-    	else
-    		i--;
+      randRow = (int)(Math.random() * SIZE);
+      randCol = (int)(Math.random() * SIZE);
+      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "S";
+      else i--;
     }
-    
     gridNoCharacter = new String[SIZE][SIZE];
-    for(int r = 0; r < SIZE; r++)
-    	for(int c = 0; c < SIZE; c++)
-    		gridNoCharacter[r][c] = grid[r][c];
+    for(int r = 0; r < SIZE; r++) for(int c = 0; c < SIZE; c++) gridNoCharacter[r][c] = grid[r][c];
     gridNoCharacter[middle][middle] = "A";
-    
-  }
+  }//Map() constructor end
   
   public String[][]getData(){return grid;}
   public int getPlayerX(){return playerX;}
@@ -103,32 +80,35 @@ public class Map {
     if (d == "BACKWARD" || d == "DOWN") playerY++;
     if (d == "LEFT") playerX--;
     if (d == "RIGHT") playerX++;
+    if (d != "FORWARD" || d != "UP" || d != "BACKWARD" || d != "DOWN" || d != "LEFT" || d != "RIGHT")
+      throw new Error(d+" is not a direction");
     grid[playerY][playerX] = "@";
     grid[tempY][tempX] = gridNoCharacter[tempY][tempX];
   }
 
   public String toString(){
     String ans = "";
-    for(int r = 0; r < grid.length; r++)
-    {
-    	for(int c = 0; c < grid[r].length; c++)
-    	{
-    		ans += grid[r][c] + " ";
-    	}
-    	ans += "\n";
+    for(String[]i : grid){
+      for(String s : i) ans += s;
+      ans += "\n";
     }
     return ans;
   }
   
   public static void main(String[] args)
   {
-	  Map test = new Map();
-	  test.movePlayer("RIGHT");
-	  test.movePlayer("RIGHT");
-	  test.movePlayer("UP");
-	  test.movePlayer("RIGHT");
-	  test.movePlayer("RIGHT");
-	  System.out.println(test);
+    Map test = new Map();
+    test.movePlayer("RIGHT"); System.out.println(test);
+    test.movePlayer("RIGHT"); System.out.println(test);
+    test.movePlayer("UP");    System.out.println(test);
+    test.movePlayer("RIGHT"); System.out.println(test);
+    test.movePlayer("RIGHT"); System.out.println(test);
+    test.movePlayer("UP");    System.out.println(test);
+    test.movePlayer("LEFT");  System.out.println(test);
+    test.movePlayer("LEFT");  System.out.println(test);
+    test.movePlayer("DOWN");  System.out.println(test);
+    test.movePlayer("LEFT");  System.out.println(test);
+    test.movePlayer("LEFT");  System.out.println(test);
+    test.movePlayer("DOWN");  System.out.println(test);
   }
 }
-
