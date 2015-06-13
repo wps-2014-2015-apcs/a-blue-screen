becaab43bcd224adf30e1f2ffac8320108a86357
haelmic <haelmic3@gmail.com>
2015-06-13 00:04:03 -0400

    Map
    Stuff...
    Ok I'll tell you
    +Map(Str[][])-gridNoCharicter+player(@):toString+stuff

--- src/Map.java
+++ src/Map.java
@@ -13,24 +13,28 @@
 |*|@author Ashley Yu
 |*|@author Jeremy Yu
 |*|
+|*|This is the map and the methods for moving around the map
+|*|
 \*/
-
-/**
- * This is the map and the methods for moving around the map
- *
- */
+  
 public class Map {
-  public String[][] grid;
-  public String[][] gridNoCharacter;
-//public int[][] screen;
-  public int playerX;
-  public int playerY;
-  private String[][] school = {
-    {"?","?","?","?"},
-    {"?","?","?","?"},
-    {"?","?","?","?"},
-    {"?","?","?","?"}
-  };  
+  
+//fields  
+  private String[][] grid;
+  private String[][] gridHelp;
+  //public int[][] screen;
+  private int playerX;
+  private int playerY;
+  private static String[][] school = {
+    {"?","?","?","?","?"},
+    {"?","?","?","?","?"},
+    {"?","?","?","?","?"},
+    {"?","?","?","?","?"},
+    {"?","?","?","?","?"}
+  };
+  private boolean spaces = true;
+  
+//constructors
   public Map(){
     final int SIZE = 21;
     int m = (SIZE - 1) / 2;
@@ -38,71 +42,77 @@ public class Map {
     grid = new String[SIZE][SIZE];
     for(int r = 0; r < SIZE; r++) for(int c = 0; c < SIZE; c++) grid[r][c] = "*";
     grid[m][m] = "@";
-    int randRow;
-    int randCol;
-    for(int i = 0; i < 5; i++)// 5 Friends Houses
-    {
-      randRow = (int)(Math.random() * SIZE);
-      randCol = (int)(Math.random() * SIZE);
-      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "H";
-      else i--;
-    }
-    for(int i = 0; i < 5; i++)// 5 Coffee Shops
-    {
-      randRow = (int)(Math.random() * SIZE);
-      randCol = (int)(Math.random() * SIZE);
-      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "C";
-      else i--;
-    }
-    for(int i = 0; i < 5; i++)// 5 Office supply Stores
-    {
-      randRow = (int)(Math.random() * SIZE);
-      randCol = (int)(Math.random() * SIZE);
-      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "O";
-      else i--;
-    }
-    for(int i = 0; i < 1; i++)// 1 School
-    {
-      randRow = (int)(Math.random() * SIZE);
-      randCol = (int)(Math.random() * SIZE);
-      if(grid[randRow][randCol].equals("*")) grid[randRow][randCol] = "S";
-      else i--;
-    }
-    gridNoCharacter = new String[SIZE][SIZE];
-    for(int r = 0; r < SIZE; r++) for(int c = 0; c < SIZE; c++) gridNoCharacter[r][c] = grid[r][c];
-    gridNoCharacter[m][m] = "A";
+    build("H", 5);// 5 Friends Houses
+    build("C", 5);// 5 Coffee Shops
+    build("O", 5);// 5 Office supply Stores
+    build("E", 1);// 1 School
+    grid[m][m] = "A";
   }//Map() constructor end
+  public Map(String[][] grid){
+    final int SIZE = grid.length;
+    int m = SIZE/2;
+    this.grid = grid;
+    playerX = m; playerY = m;
+  }
+  public Map(String[][] grid , int x, int y){
+    this.grid = grid;
+    playerX = x; playerY = y;
+  }
   
+//constructor helpers 
+  private void build(String b,int n){//String Building int number
+    int SIZE = grid.length;
+    for(int i = 0; i < n; i++)
+    {
+    int randRow = (int)(Math.random() * SIZE);
+    int randCol = (int)(Math.random() * SIZE);
+      if(grid[randRow][randCol].equals("*"))
+        grid[randRow][randCol] = b;
+      else i--;
+    }
+  }
+  
+//public get methods
   public String[][]getData(){return grid;}
   public int getPlayerX(){return playerX;}
   public int getPlayerY(){return playerY;}
   
-  public void movePlayer(String d){
+//public set methods
+  public String movePlayer(String d){
+    String ans = "";
     d = d.toUpperCase();
-    int tempX = playerX;
-    int tempY = playerY;
     if (d == "FORWARD" || d == "UP") playerY--;
     if (d == "BACKWARD" || d == "DOWN") playerY++;
     if (d == "LEFT") playerX--;
     if (d == "RIGHT") playerX++;
     if (d != "FORWARD" && d != "UP" && d != "BACKWARD" && d != "DOWN" && d != "LEFT" && d != "RIGHT")
       throw new Error(d+" is not a direction");
-    grid[playerY][playerX] = "@";
-    grid[tempY][tempX] = gridNoCharacter[tempY][tempX];
+    if (playerX >= grid.length){playerX--; ans = "YOU ARE TOO FAR OUT";}
+    if (playerY >= grid.length){playerY--; ans = "YOU ARE TOO FAR OUT";}
+    if (playerX < 0){playerX++; ans = "YOU ARE TOO FAR OUT";}
+    if (playerY < 0){playerY++; ans = "YOU ARE TOO FAR OUT";}
+    return ans;
   }
-
+  
+//print methods
   public String toString(){
     String ans = "";
-    for(String[]i : grid){
-      for(String s : i) ans +=" " + s + " ";
+    for(int i = 0; i < grid.length; i++){
+      for(int j = 0; j < grid[i].length; j++){ 
+        String s = grid[i][j];
+        if(i == playerY && j == playerX)s = "@";
+        if(spaces)ans +=" " + s + " ";
+        else ans += s;
+      }
       ans += "\n";
     }
     return ans;
   }
   
+//main
   public static void main(String[] args)
   {
-    Map test = new Map();
+    Map test = new Map(school);
     test.movePlayer("RIGHT"); System.out.println(test);
     test.movePlayer("RIGHT"); System.out.println(test);
     test.movePlayer("UP");    System.out.println(test);
