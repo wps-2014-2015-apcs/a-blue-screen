a08136e7d30abc0f5b2c7d66073906e504b82a85
haelmic <haelmic3@gmail.com>
2015-06-18 14:07:28 -0400

    Storyline Update
    put the strings in the functions into the strings

--- src/Storyline.java
+++ src/Storyline.java
@@ -20,66 +20,66 @@
 
 public class Storyline
 {
-	
-	public static String beginning; // you know what I mean
-	public static String doHomework; // first time doing your HW
-	public static String pressDoHomework; // all time but first time
-	public static String chargeLaptop; // charge laptop
-	public static String coldRoom; // when you click "An Uncomfortable Old Room"
-	public static String writeCode; // when you press "Write Code"
-	public static String noPencil; // when you press "Do Homework," and no pencil
-	public static String officeStore; // when you buy pencil
-	public static String drinkCoffee; // when you drink coffee
-	public static String drinkOpenLid; // when you drink open-lid coffee
-	public static String playGame; // when you play game
-	public static String win; // when you get grade above A-
-	public static String lose; // when you get grade lower than C+
-	public static String ratAttack; // when you are attacked by rats (...)
+ 
+ public static String beginning = "You wake up from your dream, flustered and confused. You have fallen asleep at your desk, drool running across your chin. It is Sunday night, and your APCS homework is due. You need to do homework"; // you know what I mean
+ public static String doHomework = "Doing your homework has improved your grade by two points. You now have a 62."; // first time doing your HW
+ public static String pressDoHomework = "Your assignment is finished."; // all time but first time
+ public static String chargeLaptop = "laptop increase by 25%."; // charge laptop
+ public static String coldRoom = "In this classroom you will write code and take tests. Beware of stress level."; // when you click "An Uncomfortable Old Room"
+ public static String writeCode = "There is progress on your code. Progress on your code."; // when you press "Write Code"
+ public static String noPencil = "You are out of pencils. The office supply store may supply you with more."; // when you press "Do Homework," and no pencil
+ public static String officeStore = "Pencils galore. Take what you need."; // when you buy pencil
+ public static String drinkCoffee = "Maybe now you can stay awake... Health increase!"; // when you drink coffee
+ public static String drinkOpenLid = "Even better! Health increase!"; // when you drink open-lid coffee
+ public static String playGame = "Health increase!"; // when you play game
+ public static String win = "You are a moderately decent student! Congrats!"; // when you get grade above A-
+ public static String lose = "You will get nowhere in life."; // when you get grade lower than C+
+ public static String ratAttack = "You will get nowhere in life."; // when you are attacked by rats (...)
 
-	//test comment
-	public String beginning()
-	{return "You wake up from your dream, flustered and confused. You have fallen asleep at your desk, drool running across your chin. It is Sunday night, and your APCS homework is due. You need to do homework";}
-			
-	public String doHomework()
-	{return "Doing your homework has improved your grade by two points. You now have a 62.";}
-			
-	String pressDoHomework()
-	{return "Your assignment is finished.";}
-			
-	public String chargeLaptop()
-	{return "laptop increase by 25%.";}
-			
-	public String coldRoom()
-	{return "In this classroom you will write code and take tests. Beware of stress level.";}
-			
-	public String wrtieCode()
-	{return "There is progress on your code. Progress on your code.";}
-			
-	public String noPencil()
-	{return "You are out of pencils. The office supply store may supply you with more.";}
-			
-	public String officeStore()
-	{return "Pencils galore. Take what you need.";}
-			
-	public String drinkCoffee()
-	{return "Maybe now you can stay awake... Health increase!";}
-			
-	public String drinkOpenLid()
-	{return "Even better! Health increase!";}
-			
-	public String playGame()
-	{return "Health increase!";}
-			
-	public String win()
-	{return "You are a moderately decent student! Congrats!";}
-		
-	public String lose()
-	{return "You will get nowhere in life.";}
-			
-	public String ratAttack()
-	{return "Oh Carp! Random Rat Attack!";}
+ //test comment
+ public String beginning()
+ {return "You wake up from your dream, flustered and confused. You have fallen asleep at your desk, drool running across your chin. It is Sunday night, and your APCS homework is due. You need to do homework";}
+   
+ public String doHomework()
+ {return "Doing your homework has improved your grade by two points. You now have a 62.";}
+   
+ String pressDoHomework()
+ {return "Your assignment is finished.";}
+   
+ public String chargeLaptop()
+ {return "laptop increase by 25%.";}
+   
+ public String coldRoom()
+ {return "In this classroom you will write code and take tests. Beware of stress level.";}
+   
+ public String wrtieCode()
+ {return "There is progress on your code. Progress on your code.";}
+   
+ public String noPencil()
+ {return "You are out of pencils. The office supply store may supply you with more.";}
+   
+ public String officeStore()
+ {return "Pencils galore. Take what you need.";}
+   
+ public String drinkCoffee()
+ {return "Maybe now you can stay awake... Health increase!";}
+   
+ public String drinkOpenLid()
+ {return "Even better! Health increase!";}
+   
+ public String playGame()
+ {return "Health increase!";}
+   
+ public String win()
+ {return "You are a moderately decent student! Congrats!";}
+  
+ public String lose()
+ {return "You will get nowhere in life.";}
+   
+ public String ratAttack()
+ {return "You will get nowhere in life.";}
 
 
 
 }
-	
+ 
